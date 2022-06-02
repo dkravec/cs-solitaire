bcbddd676d6f4e82969ff7139dc3ab0a58369050
Daniel Kravec <daniel@kravec.com>
2022-06-01 21:42:23 -0400

    1.0 (1.2022.06.01)

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Solitaire 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Deck myObj = new Deck();
+            myObj.colour = "red";
+            myObj.Run();
+            myObj.CardNum();
+            myObj.changeColor("blue");
+            Console.WriteLine(myObj.colour);
+            gameTitle();
+        }
+
+        public static void gameTitle() 
+        {
+            // Console.Clear();
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Welcome to Solitaire!");
+            Console.WriteLine("Press 'Enter' to begin!");
+            Console.ReadLine();
+            Console.Clear();
+            drawCards();
+        }
+
+        public static void drawCards() 
+        {
+            Console.Clear();
+            Console.WriteLine("Drawing cards");
+            /* deck layout
+                [?] [o]     [:] [:] [:] [:]
+
+
+                [o] [?] [?] [?] [?] [?] [?] 
+                [:] [o] [?] [?] [?] [?] [?]
+                [:] [:] [o] [?] [?] [?] [?]
+                [:] [:] [:] [o] [?] [?] [?]
+                [:] [:] [:] [:] [o] [?] [?]
+                [:] [:] [:] [:] [:] [o] [?]
+                [:] [:] [:] [:] [:] [:] [o]
+
+                o = revealed card
+                ? = hidden card
+                : = empty space
+            */
+
+        }
+
+        public static int SubNums(int a, int b)
+        {
+            int x = a - b;
+            return x;
+        }
+
+        public static void shuffleCards() 
+        {
+            Console.Clear();
+            Console.WriteLine("Shuffling cards");
+
+        }
+    }
+
+    class Deck 
+    {
+        public string colour = "blue";
+        public int numCards = 52;
+        public int numDeck = 52;
+        public int numDiscard = 0;
+        public int[] currentDiscard = { };
+
+        public void Run() {
+            Console.WriteLine(colour);
+        }
+        public void CardNum() {
+            Console.WriteLine(numCards);
+        }
+        public void changeNumDeck(int num) {
+            numDeck = num;
+        }
+        public void changeNumDiscard(int num) {
+            numDiscard = num;
+        }
+        public void changeColor(string newColour) {
+            colour = newColour;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Solitare
+Created by Daniel Kravec, on June 1st, 2022
+
+## About
+This is a project to help me learn C#, so the code may not be perfect. And some earlier commits may be confusing compared to what the final project with be, since i would be testing random stuff.
+
+## Usage
+Run "dotnet run" in the terminal with the project root folder as your entry point. this will automatically run Program.cs
+
+## Version history
+### 1.0 (1.2022.06.01)
+- created base program.
+- created a "Deck" class (also helped with learning classes in c#).
+    - this will be used to keep data and functions to change those data easier.
+- created layout for drawcards, which will be the terminal screen eventually.
+- The program basically does nothing at this moment.
